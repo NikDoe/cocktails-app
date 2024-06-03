2dccf02b62047b99f2e3fd3df9cc352d10c8634b
NikDoe <nikd0e@yandex.ru>
2024-06-03 18:29:11 +0300

    check navigation state

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 function Layout() {
+	const { state: navigationState } = useNavigation();
+
 	return (
 		<div>
 			<Navbar />
 			<section className='page'>
-				<Outlet />
+				{navigationState === 'loading' && <div>Загрузка...</div>}
+				{navigationState === 'idle' && <Outlet />}
 			</section>
 		</div>
 	);
