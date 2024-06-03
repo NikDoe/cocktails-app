89ade0a153ed9a94f2d7ae1ed23b0c3fc64924f8
NikDoe <nikd0e@yandex.ru>
2024-06-03 16:01:21 +0300

    fetch data from cocktails API

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,12 @@
+import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
 
+const cocktailsDBUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
 export const loader = async () => {
-	return 'какие-то данные';
+	const searchTerm = 'vodka';
+	const response = await axios.get(`${cocktailsDBUrl}${searchTerm}`);
+	return { drinks: response.data.drinks, searchTerm };
 };
 
 function Landing() {
