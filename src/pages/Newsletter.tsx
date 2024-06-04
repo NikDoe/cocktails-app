4af7e296ba34b2facd9b439ef5e9032967cf72a7
NikDoe <nikd0e@yandex.ru>
2024-06-04 15:47:24 +0300

    add Newsletter page structure

--- src/pages/Newsletter.tsx
+++ src/pages/Newsletter.tsx
@@ -1,6 +1,57 @@
 function Newsletter() {
 	return (
-		<h1>Newsletter</h1>
+		<form className='form'>
+			<h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        our newsletter
+			</h4>
+			{/* name */}
+			<div className='form-row'>
+				<label htmlFor='name' className='form-label'>
+					name
+				</label>
+				<input
+					type='text'
+					className='form-input'
+					name='name'
+					id='name'
+					required
+				/>
+			</div>
+			{/* lastName */}
+			<div className='form-row'>
+				<label htmlFor='lastName' className='form-label'>
+					last name
+				</label>
+				<input
+					type='text'
+					className='form-input'
+					name='lastName'
+					id='lastName'
+					required
+				/>
+			</div>
+			{/* email */}
+			<div className='form-row'>
+				<label htmlFor='email' className='form-label'>
+					email
+				</label>
+				<input
+					type='text'
+					className='form-input'
+					name='email'
+					id='email'
+					defaultValue='test@test.com'
+					required
+				/>
+			</div>
+			<button
+				type='submit'
+				className='btn btn-block'
+				style={{ marginTop: '0.5rem' }}
+			>
+				submit
+			</button>
+		</form>
 	);
 }
 
