4c80a96f4dcac4c9ebc4895d72320bdb7ea7931b
NikDoe <nikd0e@yandex.ru>
2024-06-05 17:52:02 +0300

    setup react query in landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,19 +3,30 @@ import { LoaderFunction, useLoaderData } from 'react-router-dom';
 import { TFetchDrinks } from '../types';
 import CocktailList from '../components/CocktailList';
 import SearchForm from '../components/SearchForm';
+import { useQuery } from '@tanstack/react-query';
 
 const COCKTAILS_DB_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+const searchCocktailsQuery = (searchTerm: string) => {
+	return {
+		queryKey: ['search', searchTerm || 'all'],
+		queryFn: async () => {
+			const response = await axios.get(`${COCKTAILS_DB_URL}${searchTerm}`);
+			return response.data.drinks;
+		},
+	};
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
 	const url = new URL(request.url);
 	
 	const searchTerm = url.searchParams.get('search') || '';
-	const response = await axios.get(`${COCKTAILS_DB_URL}${searchTerm}`);
-	return { drinks: response.data.drinks, searchTerm };
+	return { searchTerm };
 };
 
 function Landing() {
-	const { drinks, searchTerm } = useLoaderData() as TFetchDrinks;
+	const { searchTerm } = useLoaderData() as TFetchDrinks;
+	const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm));
 	
 	return (
 		<>
